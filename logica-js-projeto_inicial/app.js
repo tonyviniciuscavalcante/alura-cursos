3167dec4f2e96a2b8c8147a4c8aa68dfe9bd3ddc
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-06-12 00:41:59 -0300

    update app

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,11 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
 // se o chute for igual ao número secreto
 if (numeroSecreto == chute) {
-    console.log('Isso ai! VocÊ descobriu o número secreto (5)');
+    alert('Isso ai! VocÊ descobriu o número secreto (5)');
 } else {
     alert('Você errou :(')
 }
