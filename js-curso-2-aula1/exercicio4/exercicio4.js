ca3718014b48eedbd969a781af6387cd063234aa
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-06-12 23:46:13 -0300

    Create exercicio4.js

---
+++ js-curso-2-aula1/exercicio4/exercicio4.js
@@ -0,0 +1,23 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+console.log(listaGenerica);
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+console.log(linguagensDeProgramacao);
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+console.log(linguagensDeProgramacao);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaNomes = ['Tony', 'Jão', 'Alexandre'];
+console.log(listaNomes[0]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(listaNomes[1]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(listaNomes[2]);
